5efd30b035e88e75e61b571acdf4f5af85a04fc8
Emily Ho <eeho2@illinois.edu>
2022-11-16 12:30:30 -0600

    MakeFile

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "./data/convert.py"
+
+int main() {
+    std::cout << "main function" << std::endl;
+    // convert_airports_csv();
+    // convert_routes_csv();
+    return 0;
+}

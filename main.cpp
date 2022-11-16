6eb386c46517467f41287e47526f7352f0f9d759
Emily Ho <eeho2@illinois.edu>
2022-11-16 13:04:22 -0600

    trying ti fix python errors

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     std::cout << "main function" << std::endl;
+    // figure out how to call these python files
     // convert_airports_csv();
     // convert_routes_csv();
     return 0;

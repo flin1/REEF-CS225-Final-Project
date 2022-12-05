44faa26734fbee419c16395626339fb5e3b42ddf
Emily Ho <eeho2@illinois.edu>
2022-12-04 22:09:49 -0600

    Edited makefile and parsing.py and main()

--- data/convert.py
+++ data/convert.py
@@ -1,8 +1,11 @@
 '''Convert routes and airport files to a usable CSV file'''
 import pandas as pd
+
+
 def convert_airports_csv():
     '''Adds field names and filters out airports.csv'''
-    df_airports = pd.read_csv ('old_airports.csv', header = None)
+    df_airports = pd.read_csv ("/Users/emilyho/Desktop/REEF-CS225-Final-Project/data/old_airports.csv", header = None)
+    # df_airports = pd.read_csv ('../data/old_airports.csv', header = None)
     df_airports.columns = ['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO',
         'Latitude', 'Longitude', 'Altitude', 'Timezone', 'DST',
         'TZ Database Time Zone', 'Type', 'Source']
@@ -14,10 +17,15 @@ def convert_airports_csv():
     df_airports.drop('TZ Database Time Zone', axis = 1, inplace = True)
     df_airports.drop('Type', axis = 1, inplace = True)
     df_airports.drop('Source', axis = 1, inplace = True)
-    df_airports.to_csv('airports.csv', index = False)
+    
+    # df_airports = df_airports.replace('','-', regex=True)
+    df_airports['Name'] = df_airports['Name'].str.encode('ascii', 'ignore').str.decode('ascii')
+    # df_airports.drop(df_airports[df_airports['Name'] != 0].index, inplace = True)
+    df_airports = df_airports.iloc[1: , :]
+    df_airports.to_csv('airports.csv', index = False, header = False)
 def convert_routes_csv():
     '''this function adds field names to routes.csv and converts to an updated csv file'''
-    df_routes = pd.read_csv ('old_routes.csv', header = None)
+    df_routes = pd.read_csv ('/Users/emilyho/Desktop/REEF-CS225-Final-Project/data/old_routes.csv', header = None)
     df_routes.columns = ['Airline', 'Airline ID', 'Source Airport', 'Source Airport ID',
         'Destination Airport', 'Destination Airport ID', 'Codeshare', 'Stops', 'Equipment']
     df_routes.drop('Airline', axis = 1, inplace = True)
@@ -29,5 +37,9 @@ def convert_routes_csv():
     df_routes.drop(df_routes[df_routes['Source Airport ID'] == "\\N"].index, inplace = True)
     df_routes.drop(df_routes[df_routes['Destination Airport ID'] == "\\N"].index, inplace = True)
     df_routes.drop(df_routes[df_routes['Stops'] != 0].index, inplace = True)
-    df_routes.to_csv('routes.csv', index = False)
+    df_routes = df_routes.iloc[1: , :]
+    df_routes.to_csv('routes.csv', index = False, header = False)
+    
+convert_airports_csv()
+convert_routes_csv()
     

cdf341f3c81834e430b932edbc511a9f56a5eefd
Emily Ho <eeho2@illinois.edu>
2022-12-05 13:27:46 -0600

    Fix data issues and removed CITY field from files

--- data/convert.py
+++ data/convert.py
@@ -17,12 +17,18 @@ def convert_airports_csv():
     df_airports.drop('TZ Database Time Zone', axis = 1, inplace = True)
     df_airports.drop('Type', axis = 1, inplace = True)
     df_airports.drop('Source', axis = 1, inplace = True)
+    df_airports.drop('City', axis = 1, inplace = True)
     
-    # df_airports = df_airports.replace('','-', regex=True)
+    df_airports['Name'] = df_airports['Name'].apply(lambda x: x.replace(',', ''))
+    # df_airports['City'] = df_airports['City'].apply(lambda x: x.replace(',', ''))
+    df_airports['Name'] = df_airports['Name'].apply(lambda x: x.replace('"', ''))
+    # df_airports['City'] = df_airports['City'].apply(lambda x: x.replace('\'', ''))
+    df_airports['Country'] = df_airports['Country'].apply(lambda x: x.replace('"', ''))
+    
     df_airports['Name'] = df_airports['Name'].str.encode('ascii', 'ignore').str.decode('ascii')
     # df_airports.drop(df_airports[df_airports['Name'] != 0].index, inplace = True)
-    df_airports = df_airports.iloc[1: , :]
-    df_airports.to_csv('airports.csv', index = False, header = False)
+    # df_airports = df_airports.iloc[1: , :]
+    df_airports.to_csv('./data/airports.csv', index = False, header = False)
 def convert_routes_csv():
     '''this function adds field names to routes.csv and converts to an updated csv file'''
     df_routes = pd.read_csv ('/Users/emilyho/Desktop/REEF-CS225-Final-Project/data/old_routes.csv', header = None)
@@ -37,8 +43,8 @@ def convert_routes_csv():
     df_routes.drop(df_routes[df_routes['Source Airport ID'] == "\\N"].index, inplace = True)
     df_routes.drop(df_routes[df_routes['Destination Airport ID'] == "\\N"].index, inplace = True)
     df_routes.drop(df_routes[df_routes['Stops'] != 0].index, inplace = True)
-    df_routes = df_routes.iloc[1: , :]
-    df_routes.to_csv('routes.csv', index = False, header = False)
+    # df_routes = df_routes.iloc[1: , :]
+    df_routes.to_csv('./data/routes.csv', index = False, header = False)
     
 convert_airports_csv()
 convert_routes_csv()

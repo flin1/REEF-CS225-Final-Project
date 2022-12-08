2b20848b8b2845610301dc5b6d562e3d787d90ec
Emily Ho <eeho2@illinois.edu>
2022-12-08 16:49:44 -0600

    Added BFS() to main()

--- src/BFS.h
+++ src/BFS.h
@@ -6,22 +6,10 @@
 #include <queue>
 #include <vector>
 #include "graph.h"
-#include "airportGraph.h"
+#include "data_parse.h"
 #include <map>
 
 //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 
 //represents a directed graph to perform bfs traversal on
-class BFS {
-
-   public:
-    BFS(int);
-    ~BFS();
-    void addEdge(int source, int destination);
-    std::vector<std::string> BFS( int startVertex, string, string);
-
-  private:
-    int numVertices;
-    std::vector<int> *adjacency_list;
-    std::map<int, bool> mapVisited;
-};
+std::vector<int> BFSTraversal( int startVertex, std::map<int, std::vector<std::pair<ProcessCSV::AirportNode,double> > > adjList);
